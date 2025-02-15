218383467fc0b7fc75ed080a12c14a822a9d195d
Mohankumarpilli <mohankumarpilli15@gmail.com>
2025-02-15 11:45:29 +0530

    first question done

---
+++ Age_above_30.js
@@ -0,0 +1,16 @@
+// Write a function to return a list of engineers who are above 30 years of age.
+
+const data = require('./people');
+
+function age_above_30(data,age_of){
+    let val = data.reduce( (acc,employee) =>{
+        if(employee.age > 30){
+            acc.push(employee.name);
+        }
+        return acc;
+    },[]);
+    console.log(val.length);
+    return val;
+}
+
+console.log(age_above_30(data,30));
