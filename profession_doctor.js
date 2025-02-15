9f129a303eb7125119a18d6329989d2e882729e5
Mohankumarpilli <mohankumarpilli15@gmail.com>
2025-02-15 11:54:09 +0530

    done second one

--- profession_doctor.js
+++ profession_doctor.js
@@ -3,6 +3,17 @@
 
 const data = require('./people');
 
-function doctor(data,){
-
+function doctor(data,profession){
+    let answer = {};
+    
+    let profession_details = data.reduce( (acc,person) => {
+        if(person.profession == profession){
+            acc.push(person.name);
+        }
+        return acc;
+    },[]);
+    answer[profession] = profession_details;
+    return answer;
 }
+
+console.log(doctor(data,'Doctor'));
